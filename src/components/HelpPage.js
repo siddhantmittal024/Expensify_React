7f41a7dc6bf45463dc15bb0f565cc5da85a2d607
Siddhant Mittal <sidmit2001@gmail.com>
2020-11-19 22:21:21 +0530

    Delete HelpPage.js

--- src/components/HelpPage.js
+++
@@ -1,6 +0,0 @@
-import { Switch, BrowserRouter, Route, Link, NavLink } from "react-router-dom";
-import React from "react";
-
-const HelpPage = () => <div>This is from my help page.</div>;
-
-export default HelpPage;
