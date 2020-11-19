ab99f33d3b89802b0c2c5cc41e899d868e8c40d9
Siddhant Mittal <sidmit2001@gmail.com>
2020-11-19 22:19:32 +0530

    Delete ExpenseDashboardPage.js

--- src/components/ExpenseDashboardPage.js
+++
@@ -1,6 +0,0 @@
-import { Switch, BrowserRouter, Route, Link, NavLink } from "react-router-dom";
-import React from "react";
-
-const ExpenseDashboardPage = () => <div>This is from my DashboardPage.</div>;
-
-export default ExpenseDashboardPage;
