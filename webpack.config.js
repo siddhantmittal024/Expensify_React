761e5ed378071f7b61994b31a9004bfe03828255
Siddhant Mittal <sidmit2001@gmail.com>
2020-12-03 21:21:01 +0530

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env == 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
   entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
@@ -15,13 +20,32 @@ module.exports = {
       },
       {
         test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }),
       },
     ],
   },
-  devtool: "cheap-module-eval-source-map",
+  plugins: [
+    CSSExtract
+  ],
+  devtool: isProduction ? 'source-map' : "inline-source-map",
   devServer: {
     contentBase: path.join(__dirname, "public"),
     historyApiFallback: true,
   },
 };
+}
