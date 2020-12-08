c61fd09b93fc590ce1f330f648163c78eebfe0dd
Siddhant Mittal <sidmit2001@gmail.com>
2020-12-08 22:31:23 +0530

    Add files via upload

---
+++ src/playground/promises.js
@@ -0,0 +1,16 @@
+const promise = new Promise((resolve,reject) => {
+    setTimeout(()=>{
+        reject('This is my resolved data!');
+
+    },  1500);
+});
+
+console.log('before');
+
+promise.then((data)=> {
+    console.log(data);
+}).catch((error)=> {
+    console.log('error: ',error);
+});
+
+console.log('after');
