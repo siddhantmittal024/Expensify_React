fb837b889c2ebbd91e1973d4f015363f1a1f03f1
Siddhant Mittal <sidmit2001@gmail.com>
2020-12-12 21:50:42 +0530

    Add files via upload

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,7 +4,6 @@ import {SingleDatePicker} from 'react-dates';
 
 // const date = new Date();
 const now = moment();
-console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
     constructor(props) {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
-import { Switch, BrowserRouter, Route, Link, NavLink } from "react-router-dom";
-import React from "react";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      ExpenseHelp
-    </NavLink>
+    <NavLink to="/dashboard" activeClassName="is-active" >Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <button onClick={startLogout}>Logout</button>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
